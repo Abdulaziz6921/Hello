2e5874f96b60612921a7624160067c461ece2396
Abdulaziz6921 <abdulaziztojibayev6@gmail.com>
2023-04-12 10:04:15 +0500

    fixed useEffect issues

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
-import MainPage from "./Pages/Home";
 function App() {
-  return (
-    <Login />
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Login />} />
-    //     <Route path="/Home" element={<MainPage />} />
-    //   </Routes>
-    // </BrowserRouter>
-  );
+  return <Login />;
 }
 
 export default App;
